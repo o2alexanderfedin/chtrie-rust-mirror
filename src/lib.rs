a4df33e78cf28c42394e4b41f46918dee74ecae8
Alex Fedin <alex_fedin@hotmail.com>
2026-07-25 14:53:31 -0700

    Publish chtrie safe-uplift Rust mirror @ clang2rust 0.10.0-3299-g9fb6b28f

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,10 @@ mod chtrie;
 mod chtrie_h;
 
 extern "C" {
-    fn __transpiler_isa(child: i32, ancestor: i32)
-    -> bool;
-    fn free(_: *mut ())
-    -> ();
-    fn __error()
-    -> *mut i32;
-    fn calloc(__count: u64, __size: u64)
-    -> *mut ();
-    fn malloc(__size: u64)
-    -> *mut ();
-    fn __builtin_unreachable()
-    -> ();
+    fn __transpiler_isa(child: i32, ancestor: i32) -> bool;
+    fn free(_: *mut ()) -> ();
+    fn __error() -> *mut i32;
+    fn calloc(__count: u64, __size: u64) -> *mut ();
+    fn malloc(__size: u64) -> *mut ();
+    fn __builtin_unreachable() -> ();
 }
